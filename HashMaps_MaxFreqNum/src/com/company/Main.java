50d481f41ca33def2b6cd53c2d8a94d539f54dad
SAMARTH JAIN <73464693+sjsamarthjain@users.noreply.github.com>
2021-03-17 09:44:11 +0530

    Add files via upload

---
+++ HashMaps_MaxFreqNum/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+	//You are given an array of integers that contain numbers in random order. Write a program to find and return
+        // the number which occurs the maximum times in the given input.
+        //If two or more elements contend for the maximum frequency, return the element which occurs in the array
+        // first.
+    }
+}
+
